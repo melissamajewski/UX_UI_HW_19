18416c04c255625fd3534b27a45e4ac7437b82f5
melissamajewski <153643907+melissamajewski@users.noreply.github.com>
2024-06-18 08:19:12 -0700

    Commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 console.log("Your index.js file is loaded correctly!")
+
+document.getElementById('scrollToSection').addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1); // Get the target ID without the '#'
+    smoothScroll(targetId); // Call smoothScroll function with target ID
+  });
+
+
+
+
+
+
