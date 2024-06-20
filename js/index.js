20111374ca3aa6c1c183cb37e7cb86e761f73c76
melissamajewski <153643907+melissamajewski@users.noreply.github.com>
2024-06-20 14:38:52 -0700

    Final Push

--- js/index.js
+++ js/index.js
@@ -1,14 +1,7 @@
 console.log("Your index.js file is loaded correctly!")
 
-document.addEventListener('DOMContentLoaded', function() {
-  // Function to handle smooth scrolling
-  function smoothScroll(targetId) {
-    const targetElement = document.getElementById(targetId);
-    if (targetElement) {
-      targetElement.scrollIntoView({
-        behavior: 'smooth'
-      });
-    }
+/* Loaded Correctly!!*/
+
 
 
 
